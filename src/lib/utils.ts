b2c78781715e9d4235dc095310504904a4cb9f3c
Andy Blackledge <andyblackledge@hotmail.com>
2022-09-19 16:00:06 +0100

    Updated utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,10 @@
 import fetch from 'node-fetch';
 import { customAlphabet } from 'nanoid';
+import { S3 } from 'aws-sdk';
+import { PutObjectRequest } from 'aws-sdk/clients/s3';
 
 const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', 16);
+const s3 = new S3();
 
 export const fetchFromUrlAsync = async <T>(url: string): Promise<T> => {
   const fetchResponse = await fetch(url);
@@ -12,4 +15,41 @@ export const generateQuoteReference = (): string => {
   const todayDateString = new Date().toISOString().slice(0, 10);
   const reference = `${todayDateString}-${nanoid().slice(0, 9)}`;
   return reference;
+};
+
+export const putDataInS3Async = async ({
+  bucketName,
+  key,
+  data,
+  expirySeconds,
+}: {
+  bucketName?: string;
+  key: string;
+  data: string;
+  expirySeconds?: number;
+}): Promise<string> => {
+  //
+  if (bucketName === undefined) throw new Error('bucketName === undefined');
+
+  const s3Params = {
+    Bucket: bucketName,
+    Key: key,
+  };
+
+  await s3
+    .putObject({
+      ...s3Params,
+      ACL: 'bucket-owner-full-control',
+      Body: data,
+    } as PutObjectRequest)
+    .promise();
+
+  // https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-presigned-url.html
+
+  const dataUrl = await s3.getSignedUrlPromise('getObject', {
+    ...s3Params,
+    Expires: expirySeconds ?? 60,
+  });
+
+  return dataUrl;
 };
--- src/request-api/RequestApi.EventPublisher.ts
+++ src/request-api/RequestApi.EventPublisher.ts
@@ -6,9 +6,11 @@ import EventBridge, {
   PutEventsRequest,
   PutEventsRequestEntry,
 } from 'aws-sdk/clients/eventbridge';
-import S3, { PutObjectRequest } from 'aws-sdk/clients/s3';
 import { randomUUID } from 'crypto';
-import { generateQuoteReference } from '../lib/utils';
+import {
+  generateQuoteReference,
+  putDataInS3Async as getDataUrlAsync,
+} from '../lib/utils';
 import {
   EventDomain,
   EventDetailType,
@@ -19,7 +21,6 @@ import {
 export const DATA_BUCKET_NAME = 'DATA_BUCKET_NAME';
 export const APPLICATION_EVENT_BUS_NAME = 'APPLICATION_EVENT_BUS_NAME';
 
-const s3 = new S3();
 const bucketName = process.env[DATA_BUCKET_NAME];
 
 const eventBridge = new EventBridge();
@@ -34,38 +35,24 @@ export const handler = async (event: APIGatewayEvent): Promise<any> => {
     };
   }
 
-  // Generate the id and reference
+  // Generate the reference
 
-  const correlationId = event.headers['x-correlation-id'] ?? randomUUID();
   const quoteReference = generateQuoteReference();
 
   // Store the body and get a pre-signed URL
 
-  const s3Params = {
-    Bucket: bucketName,
-    Key: `${quoteReference}/${quoteReference}-quote-request.json`,
-  };
-
-  await s3
-    .putObject({
-      ...s3Params,
-      ACL: 'bucket-owner-full-control',
-      Body: event.body,
-    } as PutObjectRequest)
-    .promise();
-
-  // https://docs.aws.amazon.com/AmazonS3/latest/userguide/using-presigned-url.html
-
-  const quoteRequestDataUrl = await s3.getSignedUrlPromise('getObject', {
-    ...s3Params,
-    Expires: 5 * 60, // 5 minutes
+  const quoteRequestDataUrl = await getDataUrlAsync({
+    bucketName,
+    key: `${quoteReference}/${quoteReference}-quote-request.json`,
+    data: event.body,
+    expirySeconds: 5 * 60, // 5 minutes
   });
 
   // Publish the event to process the quote
 
   const quoteSubmitted: QuoteSubmitted = {
     metadata: {
-      correlationId,
+      correlationId: event.headers['x-correlation-id'] ?? randomUUID(),
       requestId: event.requestContext.requestId,
       domain: EventDomain.LoanBroker,
       service: EventService.RequestApi,
@@ -88,6 +75,7 @@ export const handler = async (event: APIGatewayEvent): Promise<any> => {
   };
 
   const response = await eventBridge.putEvents(request).promise();
+
   console.log(JSON.stringify({ response }, null, 2));
 
   // Return the reference
